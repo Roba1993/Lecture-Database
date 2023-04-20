753b1837aeeb7798f3d051d0405440262fb5aff1
Robert Schütte <schuetterobert@gmail.com>
2023-04-20 10:50:58 +0200

    Added sql create

--- sql/06_create.sql
+++ sql/06_create.sql
@@ -22,3 +22,26 @@ CREATE TABLE user_lecture (
 /* View table schema */
 \d user_lecture
 
+INSERT INTO lecture 
+(name) 
+VALUES
+('Datenbanken'),
+('It-Security'),
+('Mathe');
+
+INSERT INTO user_lecture 
+(user_id, lecture_id) 
+VALUES
+(1, 1),
+(1,3),
+(2,1);
+
+INSERT INTO users 
+(firstname, lastname, birthday, student) 
+VALUES
+('Flo', 'Hamer', now(), true);
+
+INSERT INTO lecture 
+(name) 
+VALUES
+('Englisch');
