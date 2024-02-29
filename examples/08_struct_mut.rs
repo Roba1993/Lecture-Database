6224f119ffc987a915f908425ca8300791d1b4a8
Robert Schütte <schuetterobert@gmail.com>
2024-02-29 09:45:04 +0000

    Added better struct explanation

--- examples/08_struct_mut.rs
+++ examples/08_struct_mut.rs
@@ -6,12 +6,18 @@ fn main() {
         age: 30,
         student: false,
     };
+    let mut person2 = person1.clone();
+    person2.firstname = "Flo".into();
+    person2.lastname = "Hamer".into();
+    person2.id = person2.id + 1;
+    person2.student = true;
 
     println!("Person 1: {:?}", person1);
-    println!("Person: {} {}", person1.firstname, person1.lastname);
+    println!("Person 2: {:?}", person2);
+    println!("Total age: {:?}", person1.age + person2.age);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct User {
     id: i64,
     firstname: String,
